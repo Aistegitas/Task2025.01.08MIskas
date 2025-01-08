397339803c4ba5beaedf62e6df8db3ba740b182f
Aistegitas <ajjstee@gmail.com>
2025-01-08 22:38:51 +0200

    Uzduotis 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        Azuolas azuolas = new Azuolas();
+        Berzas berzas = new Berzas();
+        Egle egle = new Egle();
+        Pusis pusis = new Pusis();
+        Kadagys kadagys = new Kadagys();
 
+        azuolas.turi();
+        berzas.turi();
+        egle.turi();
+        pusis.turi();
+        kadagys.turi();
     }
 }
